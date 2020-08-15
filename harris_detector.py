d056fb01c4a785526292f54feeb41f7153016e1b
tayyabnasem <69724934+tayyabnasem@users.noreply.github.com>
2020-08-15 20:26:31 +0500

    Update harris_detector.py

--- harris_detector.py
+++ harris_detector.py
@@ -25,34 +25,3 @@ res = np.int0(res)
 img[res[:,1],res[:,0]]=[0,0,255]
 img[res[:,3],res[:,2]] = [0,255,0]
 cv2.imwrite('subpixel5.png',img)
-
-
-#Descriptor Features Extraction
-winSize = (64,64)
-blockSize = (16,16)
-blockStride = (8,8)
-cellSize = (8,8)
-nbins = 6
-derivAperture = 1
-winSigma = 4.
-histogramNormType = 0
-L2HysThreshold = 2.0000000000000001e-01
-gammaCorrection = 0
-nlevels = 64
-hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
-                        histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
-
-#compute(img[, winStride[, padding[, locations]]]) -> descriptors
-winStride = (8,8)
-padding = (8,8)
-
-locations = [(300,300),]
-f = open('temp.txt', 'a')
-for corner in corners:
-    locations[0] = corner
-    hist = hog.compute(img,winStride,padding,locations)
-    cornerStr = " ".join(["%.2f" % x for x in corner])
-    histStr = " ".join(["%.2f" % x for x in hist])
-    f.write(cornerStr+" "+histStr+"\n")
-
-f.close()
